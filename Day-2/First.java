6e3cdd324e229980e5a19032bc25002dcc28f870
nitesh-kumar09 <papamumma990@gmail.com>
2025-05-02 18:24:18 +0530

    Learning and teaching java Day-2 with complete program and Notes:

---
+++ Day-2/First.java
@@ -0,0 +1,14 @@
+class First {
+    public static void main(String[] args) {
+        int abc = 12;
+        double bigBalue = 12.3;
+        String bottle = "Liquid";
+        int String = 12;
+        int theTotalNumberOfItemsPurchasedInStore = 500;
+        int Integer = 10;
+        System.out.println("Value is: " + theTotalNumberOfItemsPurchasedInStore);
+        System.out.println("Is bottle me sirf : " + bottle + " Chije store kr skte hai");
+        System.out.println("The value store in String class is: " + String);
+        System.out.println("The value is : " + Integer);
+    }
+}
