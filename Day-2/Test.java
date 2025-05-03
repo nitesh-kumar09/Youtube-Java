af7d406ba75fae89209d20c03d6fb9585be20f10
nitesh-kumar09 <papamumma990@gmail.com>
2025-05-03 21:34:13 +0530

    Day-2 program and Notes done!

---
+++ Day-2/Test.java
@@ -0,0 +1,31 @@
+class Test {
+    public static void main(String[] args) {
+        String yourName = "ApnaAlgo";
+        String your12Name = "ApnaAlgo";
+        int _num = 12;
+        int $num = 999;
+
+        // int class = 90; Don't use keyword as identifiers
+
+        int number = 10;
+        int Number = 12;
+        int NUMBER = 14;
+
+        String String = "Nitesh Singh";
+        int Runnable = 888;
+
+        System.out.println("My Youtube name is: " + your12Name);
+        System.out.println("Value is: " + _num);
+        System.out.println("Value is: " + $num);
+        System.out.println("Value is: " + number);
+        System.out.println("Value is: " + Number);
+        System.out.println("Value is: " + NUMBER);
+
+        System.out.println(String);
+        System.out.println(Runnable);
+        
+
+    }
+}
+
+// a =! A
